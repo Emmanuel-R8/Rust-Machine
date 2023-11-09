a681f0ebad9744e8cf7bd6a0823f2fd8fe192756
Emmanuel <emmanuel.rialland@gmail.com>
2023-11-09 17:52:12 +0800

    List instructions reformatting

--- src/emulator/instructions/list.rs
+++ src/emulator/instructions/list.rs
@@ -3,39 +3,54 @@ use super::common::{ Instruction, InstructionFamily, InstructionFormat };
 pub fn make_instructions_list() -> Vec<Instruction<'static>> {
     return vec![
         Instruction::new()
+            .set_name("car")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o000)
-            .set_name("car"),
+            .set_arg_count(1)
+            .set_ret_count(1),
         Instruction::new()
+            .set_name("cdr")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o001)
-            .set_name("cdr"),
+            .set_arg_count(1)
+            .set_ret_count(1),
         Instruction::new()
+            .set_name("set-to-car")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o140)
-            .set_name("set-to-car"),
+            .set_arg_count(1)
+            .set_ret_count(0),
         Instruction::new()
+            .set_name("set-to-cdr")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o141)
-            .set_name("set-to-cdr"),
+            .set_arg_count(1)
+            .set_ret_count(0),
         Instruction::new()
+            .set_name("set-to-cdr-push-car")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o142)
-            .set_name("set-to-cdr-push-car"),
+            .set_arg_count(1)
+            .set_ret_count(1),
+
         Instruction::new()
+            .set_name("rplaca")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o200)
-            .set_name("rplaca"),
+            .set_arg_count(2)
+            .set_ret_count(0),
         Instruction::new()
+            .set_name("rplacd")
             .set_family(InstructionFamily::List)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o201)
-            .set_name("rplacd")
+            .set_arg_count(2)
+            .set_ret_count(0)
     ];
 }
