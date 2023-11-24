b353ce3e55c3d902b85232039d1f31f1c00aef20
Emmanuel <emmanuel.rialland@gmail.com>
2023-11-24 12:42:51 +0800

    More exec template files

--- src/emulator/instructions/exec_interruptible.rs
+++ src/emulator/instructions/exec_interruptible.rs
@@ -2,7 +2,16 @@ use crate::hardware::machine::VirtualMachine;
 
 // Instruction add
 impl VirtualMachine {
-    pub fn cpu_interuptible(&mut self) -> &mut Self {
+    pub fn cpu_rgetf(&mut self) -> &mut Self {
         return self;
     }
+
+    pub fn cpu_member(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_assoc(&mut self) -> &mut Self {
+        return self;
+    }
+
 }
