d705dd28048dbbbb90c5b77f1293addce1c7b821
Emmanuel <emmanuel.rialland@gmail.com>
2023-11-24 12:51:02 +0800

    More exec template files

--- src/emulator/instructions/exec_predicate.rs
+++ src/emulator/instructions/exec_predicate.rs
@@ -2,7 +2,99 @@ use crate::hardware::machine::VirtualMachine;
 
 // Instruction add
 impl VirtualMachine {
-    pub fn cpu_predicate(&mut self) -> &mut Self {
+    pub fn cpu_eq(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_eq_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_eql(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_eql_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_equal_number(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_equal_number_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_greaterp(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_greaterp_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_lesserp(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_lesserp_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_logtest(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_logtest_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_1(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_2(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_3(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_4(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_1_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_2_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_3_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_type_member_4_no_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_endp(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_plusp(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_minusp(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_zerop(&mut self) -> &mut Self {
         return self;
     }
 }
