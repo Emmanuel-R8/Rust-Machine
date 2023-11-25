3d56de85648e780e3620f94ebc0cf65692f8ee50
Emmanuel <emmanuel.rialland@gmail.com>
2023-11-25 13:48:59 +0800

    More exec template files - field extraction

--- src/emulator/instructions/def_field_extraction.rs
+++ src/emulator/instructions/def_field_extraction.rs
@@ -1,3 +1,5 @@
+use crate::hardware::machine::VirtualMachine;
+
 use super::common::{ Instruction, InstructionFamily, InstructionFormat };
 
 pub fn make_instructions_field_extraction() -> Vec<Instruction> {
@@ -6,41 +8,49 @@ pub fn make_instructions_field_extraction() -> Vec<Instruction> {
             .set_name("ldb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o170),
+            .set_opcode(0o170)
+            .set_exec(Some(VirtualMachine::cpu_ldb)),
         Instruction::new()
             .set_name("dpb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o370),
+            .set_opcode(0o370)
+            .set_exec(Some(VirtualMachine::cpu_dpb)),
         Instruction::new()
             .set_name("char-ldb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o171),
+            .set_opcode(0o171)
+            .set_exec(Some(VirtualMachine::cpu_char_ldb)),
         Instruction::new()
             .set_name("char-dpb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o371),
+            .set_opcode(0o371)
+            .set_exec(Some(VirtualMachine::cpu_char_dpb)),
         Instruction::new()
             .set_name("%p-ldb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o172),
+            .set_opcode(0o172)
+            .set_exec(Some(VirtualMachine::cpu_p_ldb)),
         Instruction::new()
             .set_name("%p-dpb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o372),
+            .set_opcode(0o372)
+            .set_exec(Some(VirtualMachine::cpu_p_dpb)),
         Instruction::new()
             .set_name("%p-tag-ldb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
-            .set_opcode(0o173),
+            .set_opcode(0o173)
+            .set_exec(Some(VirtualMachine::cpu_p_tag_ldb)),
         Instruction::new()
             .set_name("%p-tag-dpb".to_string())
             .set_family(InstructionFamily::FieldExtraction)
             .set_format(InstructionFormat::FieldExtraction)
             .set_opcode(0o373)
+            .set_exec(Some(VirtualMachine::cpu_p_tag_dpb))
     ];
 }
--- src/emulator/instructions/exec_field_extraction.rs
+++ src/emulator/instructions/exec_field_extraction.rs
@@ -5,4 +5,36 @@ impl VirtualMachine {
     pub fn cpu_field_extraction(&mut self) -> &mut Self {
         return self;
     }
+
+    pub fn cpu_ldb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_dpb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_char_ldb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_char_dpb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_p_ldb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_p_dpb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_p_tag_ldb(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_p_tag_dpb(&mut self) -> &mut Self {
+        return self;
+    }
 }
