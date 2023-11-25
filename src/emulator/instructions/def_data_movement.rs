e9c36b4eff49dfa78152b9a1d2d6cbf1bc165b4e
Emmanuel <emmanuel.rialland@gmail.com>
2023-11-25 13:41:55 +0800

    More exec template files

--- src/emulator/instructions/def_data_movement.rs
+++ src/emulator/instructions/def_data_movement.rs
@@ -1,3 +1,5 @@
+use crate::hardware::machine::VirtualMachine;
+
 use super::common::{ Instruction, InstructionFamily, InstructionFormat };
 
 pub fn make_instructions_data_movement() -> Vec<Instruction> {
@@ -6,51 +8,70 @@ pub fn make_instructions_data_movement() -> Vec<Instruction> {
             .set_name("push".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o100),
+            .set_opcode(0o100)
+            .set_exec(Some(VirtualMachine::cpu_push)),
+
         Instruction::new()
             .set_name("pop".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o340),
+            .set_opcode(0o340)
+            .set_exec(Some(VirtualMachine::cpu_pop)),
+
         Instruction::new()
             .set_name("movem".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o341),
+            .set_opcode(0o341)
+            .set_exec(Some(VirtualMachine::cpu_movem)),
+
         Instruction::new()
             .set_name("push-n-nils".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o101),
+            .set_opcode(0o101)
+            .set_exec(Some(VirtualMachine::cpu_push_n_nils)),
+
         Instruction::new()
             .set_name("push-address".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o150),
+            .set_opcode(0o150)
+            .set_exec(Some(VirtualMachine::cpu_push_address)),
+
         Instruction::new()
             .set_name("set-sp-to-address".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o151),
+            .set_opcode(0o151)
+            .set_exec(Some(VirtualMachine::cpu_set_sp_to_address)),
+
         Instruction::new()
             .set_name("set-sp-to-address-save-tos".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o152),
+            .set_opcode(0o152)
+            .set_exec(Some(VirtualMachine::cpu_set_sp_to_address_save_tos)),
+
         Instruction::new()
             .set_name("push-address-sp-relative".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o102),
+            .set_opcode(0o102)
+            .set_exec(Some(VirtualMachine::cpu_push_address_sp_relative)),
+
         Instruction::new()
             .set_name("stack-blt".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
-            .set_opcode(0o224),
+            .set_opcode(0o224)
+            .set_exec(Some(VirtualMachine::cpu_stack_blt)),
+
         Instruction::new()
             .set_name("stack-blt-address".to_string())
             .set_family(InstructionFamily::Interruptible)
             .set_format(InstructionFormat::OperandFromStack)
             .set_opcode(0o352)
+            .set_exec(Some(VirtualMachine::cpu_stack_blt_address))
     ];
 }
--- src/emulator/instructions/exec_data_movement.rs
+++ src/emulator/instructions/exec_data_movement.rs
@@ -5,4 +5,44 @@ impl VirtualMachine {
     pub fn cpu_data_movement(&mut self) -> &mut Self {
         return self;
     }
+
+    pub fn cpu_push(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_pop(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_movem(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_push_n_nils(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_push_address(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_set_sp_to_address(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_set_sp_to_address_save_tos(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_push_address_sp_relative(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_stack_blt(&mut self) -> &mut Self {
+        return self;
+    }
+
+    pub fn cpu_stack_blt_address(&mut self) -> &mut Self {
+        return self;
+    }
 }
